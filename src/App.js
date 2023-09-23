df940624c7dd6277f867c700579425b8c2517e0b
Maicol <pessoal.maicol@gmail.com>
2023-09-23 18:33:33 -0300

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [city, setCity] = useState("")
+  const [weather, setWeather] = useState(null)
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=9fd8c9ba33504acc904232452231009&q=${city}&lang=pt`
+    )
+      .then((response) => {
+        if(response.status === 200){
+          return response.json();
+        }
+       
+      })
+      .then((data) => {
+        setWeather(data)
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a className="navbar-brand text-white" href="#top">
+          Previsao do tempo
         </a>
-      </header>
+      </nav>
+
+      <main className="container">
+        <div className="jumbotron">
+          <h1>Verifique a previsão do tempo</h1>
+          <p className="lead">Digite o nome da sua cidade</p>
+        </div>
+        <div className="row mb-4">
+          <div className="col-mb-6">
+            <input
+              onChange={handleChange}
+              type="text"
+              className="form-control"
+            />
+          </div>
+        </div>
+        <button onClick={handleSubmit} className="btn btn-primary btn-lg">
+          Pesquisar
+        </button>
+        <div>
+          {weather ? <img src={weather.current.condition.icon} alt="" />: null}
+        </div>
+        <div>
+        <h3>Hoje o dia esta: {weather?weather.current.condition.text:null}</h3>
+          <p>Temp: {weather?weather.current.temp_c:null} celsius</p>
+        </div>
+      </main>
     </div>
   );
 }
-
 export default App;
